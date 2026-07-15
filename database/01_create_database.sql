75d8e15546d00a18de14dc4ffe688e1e2de4b564
B2BMatch <btwobmatch2026@gmail.com>
2026-07-15 13:46:17 -0400

    script sql 1.0.1"

--- database/01_create_database.sql
+++ database/01_create_database.sql
@@ -8,44 +8,16 @@
 */
 
 -- ============================================
--- Create Database
--- ============================================
-
-CREATE DATABASE b2bmatch
-WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
-    TEMPLATE = template0;
-
--- ============================================
--- Connect to Database
--- (Execute this manually in pgAdmin or psql)
--- ============================================
-
--- \c b2bmatch;
-
--- ============================================
 -- Recommended Schema
 -- ============================================
 
 CREATE SCHEMA IF NOT EXISTS b2bmatch;
 
-SET search_path TO b2bmatch;
+-- Establecemos el esquema por defecto para las siguientes tablas
+SET search_path TO b2bmatch, public;
 
 -- ============================================
 -- Extensions
 -- ============================================
 
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
--- ============================================
--- Verify
--- ============================================
-
--- SELECT current_database();
-
--- SELECT current_schema();
-
--- SELECT version();
