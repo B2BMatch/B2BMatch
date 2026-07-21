0399ae16d40aef2dbfa067c5491e381c2de8221e
Gustavo <gustavo.soto.cardenas1994@gmail.com>
2026-07-21 12:00:39 -0400

    Base de datos agregada a rama backend

--- BACKEND/usuarios/src/main/java/com/b2bmatch/usuarios/model/AppUser.java
+++ BACKEND/usuarios/src/main/java/com/b2bmatch/usuarios/model/AppUser.java
@@ -1,10 +1,30 @@
 package com.b2bmatch.usuarios.model;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "AppUser")
+@Table(name = "app_user")
 public class AppUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Role role_id;
+
+    private String email;
+
+    private String password_hash;
+
+    private String status;
+
+    private LocalDateTime updatedAt;
+
 }
+
+   
