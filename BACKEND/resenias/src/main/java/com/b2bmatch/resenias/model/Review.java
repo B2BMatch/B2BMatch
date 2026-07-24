a59f5905bf1161e82ff1cc4bc026ec59a6e71977
Juan Jeldres <jjeldresbiz@gmail.com>
2026-07-24 16:03:24 -0400

    modelando review

--- BACKEND/resenias/src/main/java/com/b2bmatch/resenias/model/Review.java
+++ BACKEND/resenias/src/main/java/com/b2bmatch/resenias/model/Review.java
@@ -1,10 +1,27 @@
 package com.b2bmatch.resenias.model;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "review")
+@Getter
+@Setter
 public class Review {
 
-    @id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long customer_id;
+
+    @Column
+    private Long professional_id;
 
 }
