b608b4a34fb08ec8c0acf15121d833e1b5be0b6b
Juan Jeldres <jjeldresbiz@gmail.com>
2026-07-25 19:52:17 -0400

    avance resenias

--- BACKEND/resenias/src/main/java/com/b2bmatch/resenias/service/ReviewService.java
+++ BACKEND/resenias/src/main/java/com/b2bmatch/resenias/service/ReviewService.java
@@ -1,5 +1,54 @@
 package com.b2bmatch.resenias.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.b2bmatch.resenias.model.Review;
+import com.b2bmatch.resenias.repository.ReviewRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    //ingles
+    //ver una reseña por id
+    public Review getReview(Long id) {
+
+        return reviewRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Review no encontrada"));
+        /* Review review = reviewRepository.findById(professional_id). */
+    }
+
+    //ver todas las reseñas de un profesioal
+    public List<Review> getReviewsByProfessional(Long professional_id){
+            return reviewRepository.findByProfessionalId(professional_id);
+        }
+    //reseñas de un cliente por id
+    public List<Review> getReviewsByCustomer(Long customer_id){
+        return reviewRepository.findByCustomerId(customer_id);
+    }
+
+    //crear una review
+    public Review createReview (Review review){
+        reviewRepository.save(review);
+        return review;
+    }
+
+    public Review updateReview (Long id, Review review){
+        
+        return reviewRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Review no encontrada para modificar"));
+        review.setRating(0);
+        review.setComment(null);
+        reviewRepository.save(review)
+    }
+
+    public void deletReview (Long id){
+        reviewRepository.deleteById(id);
+        //o tambien por la entidad en si reviewRepository.delete(review);
+    }
 
 }
